6b2c22ab1132e355e4ab9b1e44edd9ed8e08a2bf
João Victor Kulka <jv.kulka13@gmail.com>
2022-12-22 20:07:40 -0300

    Realizado animação da barra de rolagem.

--- script.js
+++ script.js
@@ -22,4 +22,28 @@ function toggleMenu(event) {
 }
 
 btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu); 
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+// Criação de animação da barra de rolagem do scroll
+function navegacaoAnimacaoScroll() {
+    const sections = document.querySelectorAll('.js-scroll')
+
+    if (sections.length) {
+
+        const metadeWindow = window.innerHeight * 0.8
+
+        function animaScroll() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top - metadeWindow
+
+                if (sectionTop < 0) {
+                    section.classList.add('ativo')
+                }
+            })
+        }
+        animaScroll()
+
+        window.addEventListener('scroll', animaScroll)
+    }
+}
+navegacaoAnimacaoScroll()
